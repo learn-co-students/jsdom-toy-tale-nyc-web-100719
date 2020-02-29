9c709ea7cf30b95978a15e465d9cb0f1c0ce2c87
LainaJ <lainakarosic@gmail.com>
2020-02-28 23:45:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -55,7 +55,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     postNewToy = () => {
+      fetch(`http://localhost:3000/toys`, {
 
+        headers: 
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        }
+        
+        body: JSON.stringify({
+          "name": "Jessie",
+          "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+          "likes": 0
+      }
+      )
 
     }
 
